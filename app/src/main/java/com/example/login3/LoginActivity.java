4a870e3af6b5535e840a52f41fd694a2a5249e48
tuannh <olanguyen2002@gmail.com>
2023-04-23 01:35:55 +0700

    add username

--- app/src/main/java/com/example/login3/LoginActivity.java
+++ app/src/main/java/com/example/login3/LoginActivity.java
@@ -3,17 +3,19 @@ package com.example.login3;
 import static com.google.android.material.tabs.TabLayout.GRAVITY_FILL;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.tabs.TabLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
-    private ViewPager viewPager;
+    private ViewPager2 viewPager2;
 
     float v =0;
 
@@ -24,21 +26,50 @@ public class LoginActivity extends AppCompatActivity {
 
 
         tabLayout = findViewById(R.id.tabLayoutLogin);
-        viewPager = findViewById(R.id.viewPagerLogin);
+        viewPager2 =(ViewPager2) findViewById(R.id.viewPagerLogin);
 
 //        tabLayout.addTab(tabLayout.newTab().setText("Login"));
 //        tabLayout.addTab(tabLayout.newTab().setText("Signup"));
 
+        tabLayout.addTab(tabLayout.newTab().setText("LOGIN"));
+        tabLayout.addTab(tabLayout.newTab().setText("SIGN UP"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        viewPager.setAdapter(adapter);
-//        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.setupWithViewPager(viewPager);
+        LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(),getLifecycle(),this);
+        viewPager2.setAdapter(adapter);
 
+
+
+//        viewPager2.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
 
         tabLayout.setTranslationY(300);
         tabLayout.setAlpha(v);
         tabLayout.animate().translationY(v).alpha(1).setDuration(1000).setStartDelay(100).start();
     }
+
+
 }
--- app/src/main/java/com/example/login3/LoginAdapter.java
+++ app/src/main/java/com/example/login3/LoginAdapter.java
@@ -5,49 +5,76 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-public class LoginAdapter extends FragmentStatePagerAdapter {
+public class LoginAdapter extends FragmentStateAdapter {
+    private static final int NUM_PAGES = 2;
+    private Context context;
 
-    public LoginAdapter(@NonNull FragmentManager fm, int behavior) {
-        super(fm, behavior);
+    public LoginAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle,Context context) {
+        super(fragmentManager,lifecycle);
+        this.context=context;
     }
+
+
+//    @NonNull
+//    @Override
+//    public Fragment getItem(int position) {
+//        switch (position){
+//            case 0:
+//                return new login_tab_fragment();
+//            case 1:
+//                return new Signup_tab_fragment();
+//            default:
+//                return null;
+//
+//        }
+//
+//    }
+
+//    @Override
+//    public int getCount() {
+//        return 2;
+//    }
+
+//    @Nullable
+//    @Override
+//    public CharSequence getPageTitle(int position) {
+//        String title="";
+//        switch (position){
+//            case 0:
+//                title = "LOGIN";
+//                break;
+//            case 1:
+//                title = "SIGNUP";
+//                break;
+//        }
+//        return title;
+//
+//    }
 
     @NonNull
     @Override
-    public Fragment getItem(int position) {
-        switch (position){
+    public Fragment createFragment(int position) {
+        switch (position) {
             case 0:
                 return new login_tab_fragment();
             case 1:
                 return new Signup_tab_fragment();
             default:
                 return null;
-
         }
-
     }
 
     @Override
-    public int getCount() {
-        return 2;
+    public int getItemCount() {
+        return NUM_PAGES;
     }
 
-    @Nullable
-    @Override
-    public CharSequence getPageTitle(int position) {
-        String title="";
-        switch (position){
-            case 0:
-                title = "LOGIN";
-                break;
-            case 1:
-                title = "SIGNUP";
-                break;
-        }
-        return title;
 
-    }
 }
